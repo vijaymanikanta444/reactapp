273526241bf198254cf9f7ef8e3cbe16d81f2c73
vijaymanikanta444 <vijayakp444@gmail.com>
2020-07-26 12:34:34 +0530

    class and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Wish from './components/Wish';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    show: true,
+  };
+  handleClick = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click me</button>
+        {this.state.show && <Wish name="vijay" />}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Wish.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class wish extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi {this.props.name}, how are you..</h1>
+      </div>
+    );
+  }
+}
